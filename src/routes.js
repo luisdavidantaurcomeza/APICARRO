36f6ca9dc4d1f27204120f4d49ca63b33207de4d
Luis David a Meza <luisdavid_204@hotmail.com>
2024-06-11 20:24:16 -0300

    criando o CRUD

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,14 @@ const router = express.Router();
 
 const CarroController = require('./controllers/CarroController');
 
+router.get('/carros', CarroController.buscarTodos);
 // Defina suas rotas aqui
 // Exemplo:
 // router.get('/carros', CarroController.getAllCarros);
+router.get('/carros', CarroController.buscarTodos);
+router.get('/carros/:codigo', CarroController.buscarUm);
+router.post('/carro', CarroController.inserir);
+router.put('/carro/:codigo', CarroController.alterar);
+router.delete('/carro/:codigo', CarroController.excluir);
 
 module.exports = router;
